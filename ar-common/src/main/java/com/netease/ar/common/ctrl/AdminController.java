0f925460fd912c0acf3cf25d16e797aba5ce58de
bjliqiang <bjliqiang@corp.netease.com>
2016-08-08 17:34:01 +0800

    [Feature] 登陆接口

--- ar-common/src/main/java/com/netease/ar/common/ctrl/AdminController.java
+++ ar-common/src/main/java/com/netease/ar/common/ctrl/AdminController.java
@@ -4,12 +4,17 @@ import com.google.common.collect.Maps;
 import com.netease.ar.common.http.ApiResponseBody;
 import com.netease.ar.common.http.ApiResponseBuilder;
 import com.netease.ar.common.http.intercepter.AllowNoSignature;
+import com.netease.ar.common.utils.RedisKeyUtil;
 import com.netease.ar.common.utils.SpringAppConfig;
 import org.apache.log4j.Logger;
+import org.joda.time.Seconds;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
@@ -18,6 +23,10 @@ import java.util.Map;
 @RequestMapping("/admin")
 public class AdminController {
 	private static Logger logger = Logger.getLogger(AdminController.class);
+
+	@Resource
+	private JedisPool jedisPool;
+
 
 	@RequestMapping(value = "/reversion", method = RequestMethod.GET)
 	public void reversion(HttpServletRequest request, HttpServletResponse response) {
@@ -31,6 +40,19 @@ public class AdminController {
 		Map<String, Object> map = Maps.newLinkedHashMap();
 		for (SpringAppConfig config : configs) {
 			map.put(config.getKey(), config.getValue());
+		}
+
+
+		// Redis Status
+		Jedis jedis = jedisPool.getResource();
+		try {
+			jedis.setex(RedisKeyUtil.PAINTER, 100, "1");
+			map.put("painter.jedis.status", jedis.exists(RedisKeyUtil.PAINTER));
+		} catch (Exception e){
+			map.put("painter.jedis.status", false);
+			logger.error(e);
+		} finally {
+			jedisPool.returnResource(jedis);
 		}
 		logger.info(map);
 		ApiResponseBuilder.build(response, new ApiResponseBody(map));
