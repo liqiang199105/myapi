353c991e286a2050813f99820df2d27d03e3ddb6
bjliqiang <bjliqiang@corp.netease.com>
2016-08-09 18:24:59 +0800

    [Feature] 用户手机验证码登陆Case。

--- ar-common/src/test/java/com/netease/ar/common/user/UserServiceTest.java
+++ ar-common/src/test/java/com/netease/ar/common/user/UserServiceTest.java
@@ -2,9 +2,15 @@ package com.netease.ar.common.user;
 
 
 import com.netease.ar.common.BaseTest;
+import com.netease.ar.common.http.exception.ApiError;
+import com.netease.ar.common.http.exception.ApiException;
 import com.netease.ar.common.http.intercepter.ApiSignatureUtil;
 import com.netease.ar.common.service.user.UserService;
+import com.netease.ar.common.utils.CommonUtil;
+import com.netease.vshow.special.service.SMSForBoBoWebSercice;
+import net.sf.json.JSONObject;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.util.DigestUtils;
 
 import javax.annotation.Resource;
@@ -13,6 +19,8 @@ import java.io.UnsupportedEncodingException;
 public class UserServiceTest extends BaseTest {
 
     @Resource private UserService userService;
+    @Resource @Qualifier("smsWebService") private SMSForBoBoWebSercice smsWebService;
+
 
     @Test
     public void testGetUserInfo(){
@@ -22,11 +30,23 @@ public class UserServiceTest extends BaseTest {
     @Test
     public void testRegister() throws UnsupportedEncodingException {
         String phone = "13552862889";
-        String verifyCode = "123456";
+        String verifyCode = "025311";
         userService.register(phone, verifyCode);
-//        StringBuilder sb = new StringBuilder();
-//        sb.append("123abc");
-//        System.out.println( DigestUtils.md5DigestAsHex(sb.toString().getBytes("UTF-8")).toLowerCase());
+    }
+
+    @Test
+    public void testGetUserVerifyCode() throws UnsupportedEncodingException {
+        String phone = "13552862889";
+        String verifyCode = CommonUtil.getRandomString(6, true);
+        String msg = "校验码：" + verifyCode + "，您正在验证手机号 " + phone + " 注意保密哦！";
+        JSONObject jsonObject = JSONObject.fromObject( smsWebService.sendSMS(phone, msg, "86"));
+        String respCode = jsonObject.getString("respCode");
+        if ("10000".equals(respCode)) {
+            throw new ApiException(ApiError.USER_INVALID_PHONE);
+        } else if ("400".equals(respCode)) {
+            throw new ApiException(ApiError.SMS_SEND_MESSAGE_FAILED);
+        }
+        userService.replaceVerifyCode(phone, verifyCode);
     }
 
 }
